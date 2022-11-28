80bc41d49cc0df201d782c8bc132b4a786003375
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-11-28 18:49:06 -0300

    #Commit - novos commits

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # microservice-eurekaserver-cloud
+
+## @EnableEurekaServer
+> Esta anotação fará o projeto spring boot ou microserviço agir como um agente de descoberta de serviço.
+
+> *** Atenção *** : ribbon não é mais necessário em versões mais modernas.
--- src/main/java/com/tuyo/EurekaApplication.java
+++ src/main/java/com/tuyo/EurekaApplication.java
@@ -1,9 +1,11 @@
 package com.tuyo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.netflix.eureka.server.*;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
